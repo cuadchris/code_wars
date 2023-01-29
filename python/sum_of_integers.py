7c4f8697340b953c734d1a3310abe3e33413fd61
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-01-29 11:43:58 -0500

    sum of integers in string

---
+++ python/sum_of_integers.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/598f76a44f613e0e0b000026
+
+# DESCRIPTION:
+# Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the 
+# string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+# Note: only positive integers will be tested.
+
+import re
+
+def sum_of_integers_in_string(s):
+    return sum(int(x) for x in re.findall('[0-9]+', s))
