66086602f327e59b1e02ab11eee96f965f775291
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-04-22 17:51:29 -0400

    running out of space

---
+++ python/running_out_of_space.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/56576f82ab83ee8268000059/python
+
+# DESCRIPTION:
+# Kevin is noticing his space run out! Write a function that removes the spaces from the values 
+# and returns an array showing the space decreasing.
+# For example, running this function on the array ['i', 'have','no','space'] 
+# would produce ['i','ihave','ihaveno','ihavenospace']
+
+def spacey(array):
+    result = []
+    str = ''
+    
+    for word in array:
+        str += word
+        result.append(str)
+        
+    return result
