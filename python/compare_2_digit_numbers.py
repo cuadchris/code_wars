121d22a52abc4bfdb2ef2e4c81824265e68f780d
Christopher <cuadradochris@hotmail.com>
2023-05-15 18:35:15 -0400

    compare 2 numbers

---
+++ python/compare_2_digit_numbers.py
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/63f3c61dd27f3c07cc7978de/python
+
+# DESCRIPTION:
+# You are given 2 two-digit numbers. You should check if they are similar by comparing their numbers, 
+# and return the result in %.
+
+# Example:
+
+# compare(13,14)=50%;
+# compare(23,22)=50%;
+# compare(15,51)=100%;
+# compare(12,34)=0%.
+
+def compare(a, b):
+    
+    a = [x for x in str(a)]
+    b = [x for x in str(b)]
+    matches = 0
+        
+    for x in a:
+        if x in b:
+            b.remove(x)
+            matches += 1
+
+    return f'{int(matches/2*100)}%'
+
+# overkill for sure. another busy day.
