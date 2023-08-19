f6f68f76405b2e1636046473ab2e30a1556269a3
Christopher <cuadradochris@hotmail.com>
2023-08-19 10:31:38 -0400

    quick problem for a busy day

---
+++ python/NATO_phonetic_alphabet.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/54530f75699b53e558002076/python
+
+# DESCRIPTION:
+# Complete the function word (string) and returns a string that spells the word using the 
+# NATO phonetic alphabet.
+
+# There should be a space between each word in the returned string, and the first letter 
+# of each word should be capitalized.
+
+# For those of you that don't want your fingers to bleed, this kata already has a dictionary 
+# typed out for you.
+
+# Examples
+# "hi"      -->  "Hotel India"
+# "abc"     -->  "Alpha Bravo Charlie"
+# "babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
+# "Banana"  -->  "Bravo Alpha November Alpha November Alpha"
+  
+def nato(word):
+    
+    # LETTERS is preloaded
+    return ' '.join(LETTERS[x] for x in word.upper())
