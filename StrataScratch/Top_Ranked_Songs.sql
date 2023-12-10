2ab91bc2ea3735addee725a72bb315491445db2b
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-10 10:55:22 -0500

    Top Ranked Songs | Medium

---
+++ StrataScratch/Top_Ranked_Songs.sql
@@ -0,0 +1,17 @@
+/*
+Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the
+top. Sort your records by the number of times the song was in the top position in descending order.
+
+Table: spotify_worldwide_daily_song_ranking
+*/
+
+SELECT
+    trackname,
+    count(*) AS times_top1
+FROM spotify_worldwide_daily_song_ranking
+WHERE 
+    position = 1
+GROUP BY
+    trackname
+ORDER BY
+    times_top1 DESC
