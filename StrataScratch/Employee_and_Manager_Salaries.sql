e0a37ed499bcf2fb24f780f763da8fb319fffd8b
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-11 09:45:41 -0500

    Employee and Manager Salaries

---
+++ StrataScratch/Employee_and_Manager_Salaries.sql
@@ -0,0 +1,13 @@
+/*
+Find employees who are earning more than their managers. Output the employee's first name along with the
+corresponding salary.
+
+Table: employee
+*/
+
+SELECT
+        a.first_name AS employee_name, 
+        a.salary AS employee_salary
+FROM employee a
+JOIN employee b ON  a.manager_id = b.id
+WHERE a.salary > b.salary
