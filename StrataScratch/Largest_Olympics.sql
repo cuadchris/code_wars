a6e03ebb43c7b7a595db2c63061c0c7c49a3e85e
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-12 15:13:34 -0500

    Largest Olympics | Medium

---
+++ StrataScratch/Largest_Olympics.sql
@@ -0,0 +1,20 @@
+/*
+Find the Olympics with the highest number of athletes. The Olympics game is a combination of the year and the
+season, and is found in the 'games' column. Output the Olympics along with the corresponding number of athletes.
+
+Table: olympics_athletes_events
+*/
+
+WITH cte AS (
+    SELECT games,
+        count(DISTINCT id) AS athletes_count
+    FROM olympics_athletes_events
+    GROUP BY games
+    ORDER BY athletes_count DESC
+)
+SELECT *
+FROM cte
+WHERE athletes_count = (
+        SELECT max(athletes_count)
+        FROM cte
+    )
