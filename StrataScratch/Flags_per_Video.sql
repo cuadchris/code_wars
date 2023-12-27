8266ddc922d437ff19eb34903a7207e4e3a0ff8a
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-27 11:25:46 -0500

    link

--- StrataScratch/Flags_per_Video.sql
+++ StrataScratch/Flags_per_Video.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/2102-flags-per-video?code_type=1
+
 For each video, find how many unique users flagged it. A unique user can be identified using the combination of
 their first name and last name. Do not consider rows in which there is no flag ID.
 
