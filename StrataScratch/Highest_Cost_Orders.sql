95bc4b900452d2a2afc51427c5f9e11177cac0ed
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-28 11:59:32 -0500

    Highest Cost Orders

--- StrataScratch/Highest_Cost_Orders.sql
+++ StrataScratch/Highest_Cost_Orders.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9915-highest-cost-orders?code_type=1
+
 Find the customer with the highest daily total order cost between 2019-02-01 to 2019-05-01. If customer had
 more than one order on a certain day, sum the order costs on daily basis. Output customer's first name, total
 cost of their items, and the date.
