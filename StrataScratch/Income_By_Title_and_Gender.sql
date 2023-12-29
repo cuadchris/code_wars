533dda60e3352b80f96271091f47cbadcb65e885
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-29 13:48:47 -0500

    income by title and gender

--- StrataScratch/Income_By_Title_and_Gender.sql
+++ StrataScratch/Income_By_Title_and_Gender.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/10077-income-by-title-and-gender?code_type=1
+
 Find the average total compensation based on employee titles and gender. Total compensation is calculated by
 adding both the salary and bonus of each employee. However, not every employee receives a bonus so disregard
 employees without bonuses in your calculation. Employee can receive more than one bonus.
