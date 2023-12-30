7d36ccc158cae6f02172ae50d82077266191ff75
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-12-30 14:02:29 -0500

    Number of violations | Medium

---
+++ StrataScratch/Number_of_violations.sql
@@ -0,0 +1,18 @@
+/*
+You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe'
+for each year. If an inspection resulted in a violation, there will be a value in the 'violation_id' column.
+Output the number of violations by year in ascending order.
+
+Table: sf_restaurant_health_violations
+*/
+
+SELECT EXTRACT (
+        YEAR
+        FROM inspection_date::DATE
+    ) AS YEAR,
+    count(*) AS n_inspections
+FROM sf_restaurant_health_violations
+WHERE business_name = 'Roxanne Cafe'
+    AND violation_id IS NOT NULL
+GROUP BY YEAR
+ORDER BY YEAR
