e900a6be81839a41087d2eeb14a7e919e6f78374
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-03 12:13:12 -0500

    Find matching hosts and guests

---
+++ StrataScratch/Find_matching_hosts.sql
@@ -0,0 +1,12 @@
+/*
+Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
+Output the host id and the guest id of matched pair.
+
+Tables: airbnb_hosts, airbnb_guests
+*/
+
+SELECT DISTINCT h.host_id,
+    g.guest_id
+FROM airbnb_hosts h
+    JOIN airbnb_guests g ON h.nationality = g.nationality
+    AND h.gender = g.gender
