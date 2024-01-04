c07efc3cfb95a688c5995f00e2e4744ac859e3a8
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-04 12:12:40 -0500

    Find all wineries

---
+++ StrataScratch/Find_all_wineries.sql
@@ -0,0 +1,16 @@
+/*
+Find all wineries which produce wines by possessing aromas of plum, cherry, rose, or hazelnut. To make it more
+simple, look only for singular form of the mentioned aromas. HINT: if one of the specified words is just a
+substring of another word, this should not be a hit, but a miss.
+
+
+Example Description: Hot, tannic and simple, with cherry jam and currant flavors accompanied by high, tart
+acidity and chile-pepper alcohol heat.
+Therefore the winery Bella Piazza is expected in the results.
+
+Table: winemag_p1
+*/
+
+SELECT DISTINCT winery
+FROM winemag_p1
+WHERE lower(description) ~ '\y(plum|cherry|rose|hazelnut)\y'
