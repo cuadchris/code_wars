f93cebf439600a8ac36ed4630918262cc07dedf0
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-06 11:27:35 -0500

    link

--- StrataScratch/Find_matching_hosts.sql
+++ StrataScratch/Find_matching_hosts.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/10078-find-matching-hosts-and-guests-in-a-way-that-they-are-both-of-the-same-gender-and-nationality?code_type=1
+
 Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
 Output the host id and the guest id of matched pair.
 
