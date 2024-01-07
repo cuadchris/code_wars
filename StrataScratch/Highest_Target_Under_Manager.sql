10cb829971efb416b29b4bcc3736c17b5c11e0b2
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-07 10:44:31 -0500

    more links

--- StrataScratch/Highest_Target_Under_Manager.sql
+++ StrataScratch/Highest_Target_Under_Manager.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9905-highest-target-under-manager?code_type=1
+
 Find the highest target achieved by the employee or employees who works under the manager id 13. Output the
 first name of the employee and target achieved. The solution should show the highest target achieved under
 manager_id=13 and which employee(s) achieved it.
