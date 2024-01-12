e05c15fa0b158a0e9a6fb8ac85ca3861c9bb5aa8
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-12 13:04:28 -0500

    link

--- StrataScratch/Find_the_top_10_ranked_songs_in_2010.sql
+++ StrataScratch/Find_the_top_10_ranked_songs_in_2010.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9650-find-the-top-10-ranked-songs-in-2010?code_type=1
+
 What were the top 10 ranked songs in 2010?
 Output the rank, group name, and song name but do not show the same song twice.
 Sort the result based on the year_rank in ascending order.
