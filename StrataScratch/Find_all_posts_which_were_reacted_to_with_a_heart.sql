d880ec9f92164215f8f56333ab4873d9959ab728
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-15 12:19:51 -0500

    ran out of mediums and hards

---
+++ StrataScratch/Find_all_posts_which_were_reacted_to_with_a_heart.sql
@@ -0,0 +1,10 @@
+/*
+Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+
+Tables: facebook_reactions, facebook_posts
+*/
+
+SELECT distinct p.*
+FROM facebook_posts p
+    JOIN facebook_reactions r ON p.post_id = r.post_id
+    AND r.reaction = 'heart'
