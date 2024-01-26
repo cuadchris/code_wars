e8aa76ec65b9f26fa1564b8732bc876020ed3120
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-26 14:29:18 -0500

    link

--- StrataScratch/Number_of_violations.sql
+++ StrataScratch/Number_of_violations.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-violations?code_type=1
+
 You're given a dataset of health inspections. Count the number of violation in an inspection in 'Roxanne Cafe'
 for each year. If an inspection resulted in a violation, there will be a value in the 'violation_id' column.
 Output the number of violations by year in ascending order.
