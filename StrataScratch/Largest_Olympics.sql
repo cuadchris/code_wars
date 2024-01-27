b199507b1bcef5c7feaf51d4b138c545bfbd0af8
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-27 13:25:57 -0500

    strata

--- StrataScratch/Largest_Olympics.sql
+++ StrataScratch/Largest_Olympics.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9942-largest-olympics?code_type=1
+
 Find the Olympics with the highest number of athletes. The Olympics game is a combination of the year and the
 season, and is found in the 'games' column. Output the Olympics along with the corresponding number of athletes.
 
