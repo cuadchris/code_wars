e9608bcda544b004467b632631b3ec29223df493
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-28 14:58:28 -0500

    link

--- StrataScratch/Admin_Department_Employees_Beginning_in_April_or_Later.sql
+++ StrataScratch/Admin_Department_Employees_Beginning_in_April_or_Later.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9845-find-the-number-of-employees-working-in-the-admin-department?code_type=1
+
 Find the number of employees working in the Admin department that joined in April or later.
 
 Table: worker
