d0ef3fa4eb2a9141c7a3df9015258986ba5294aa
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-30 14:52:25 -0500

    link

--- StrataScratch/Churro_Activity_Date.sql
+++ StrataScratch/Churro_Activity_Date.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9688-churro-activity-date?code_type=1
+
 Find the activity date and the pe_description of facilities with the name 'STREET CHURROS' and with a score of
 less than 95 points.
 
