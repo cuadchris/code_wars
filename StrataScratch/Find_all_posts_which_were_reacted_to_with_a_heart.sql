722fc7671483c49b56b4de089f4d9ccf4b71cc42
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-01-31 14:10:44 -0500

    link

--- StrataScratch/Find_all_posts_which_were_reacted_to_with_a_heart.sql
+++ StrataScratch/Find_all_posts_which_were_reacted_to_with_a_heart.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/10087-find-all-posts-which-were-reacted-to-with-a-heart?code_type=1
+
 Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
 
 Tables: facebook_reactions, facebook_posts
