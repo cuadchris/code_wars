b3b7237049e85779b9c3019f967c693555708775
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-02-01 11:32:26 -0500

    link

--- StrataScratch/Counting_Instances_in_Text.sql
+++ StrataScratch/Counting_Instances_in_Text.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9814-counting-instances-in-text?code_type=1
+
 Find the number of times the words 'bull' and 'bear' occur in the contents. We're counting the number of times
 the words occur so words like 'bullish' should not be included in our count.
 Output the word 'bull' and 'bear' along with the corresponding number of occurrences.
