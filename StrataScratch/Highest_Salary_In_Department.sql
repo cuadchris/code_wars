9fe62b0ebc66c3a3c068f77fe8e42750c3e7c74d
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-02-08 12:33:33 -0500

    update

--- StrataScratch/Highest_Salary_In_Department.sql
+++ StrataScratch/Highest_Salary_In_Department.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9897-highest-salary-in-department?code_type=1
+
 Find the employee with the highest salary per department.
 Output the department name, employee's first name along with the corresponding salary.
 
