cc8ef1c0380c794b6372e7304d7fedac58fd7b65
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-02-10 13:53:57 -0500

    update

--- StrataScratch/Top_Ranked_Songs.sql
+++ StrataScratch/Top_Ranked_Songs.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/9991-top-ranked-songs?code_type=1
+
 Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the
 top. Sort your records by the number of times the song was in the top position in descending order.
 
