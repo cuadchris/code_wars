7cbde900d49c9051d1e0af52687dcf9baa70c852
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-02-12 12:08:54 -0500

    cool kata

---
+++ python/thinking_testing_something_capitalized.py
@@ -0,0 +1,16 @@
+'''
+DESCRIPTION:
+
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+
+'''
+
+def testit(s):
+    
+    return ' '.join(x[:-1] + x[-1].upper() for x in s.split())
