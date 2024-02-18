79647cd4389285a36f5fd59a89e87fbda7ca4ed0
cuadchris <cuadradochris@hotmail.com>
2024-02-18 14:33:47 -0500

    Update Lyft_Driver_Wages.sql

--- StrataScratch/Lyft_Driver_Wages.sql
+++ StrataScratch/Lyft_Driver_Wages.sql
@@ -1,4 +1,6 @@
 /*
+https://platform.stratascratch.com/coding/10003-lyft-driver-wages?code_type=1
+
 Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
 Output all details related to retrieved records.
 
