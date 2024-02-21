4442d4c42799a9ea05a989d74118b902cd214f24
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-02-21 14:46:59 -0500

    Consecutive items

---
+++ python/consecutive_items.py
@@ -0,0 +1,17 @@
+'''
+DESCRIPTION:
+
+You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not
+a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr.
+'''
+
+def consecutive(arr, a, b):
+        
+    for i in range(len(arr) - 1):
+        if arr[i] == a or arr[i] == b:
+            if arr[i + 1] == a or arr[i + 1] == b:
+                return True
+            
+    return False
