8aedf368530cf23a96aeedad90bc12771f4f9c76
cuadchris <cuadradochris@hotmail.com>
2024-02-23 13:19:02 -0500

    Update float_precision.py

--- python/float_precision.py
+++ python/float_precision.py
@@ -1,12 +1,14 @@
-# https://www.codewars.com/kata/5143d157ceb46d6a61000001/python
+'''
+https://www.codewars.com/kata/5143d157ceb46d6a61000001/python
 
-# DESCRIPTION:
+DESCRIPTION:
 
-# Update the solution method to round the argument value to the closest precision of two. 
-# The argument will always be a float.
+Update the solution method to round the argument value to the closest precision of two. 
+The argument will always be a float.
 
-# 23.23456 --> 23.23
-# 1.546    --> 1.55
+23.23456 --> 23.23
+1.546    --> 1.55
+'''
 
 def solution(n):
     return int(n * 100 + 0.5) / 100.0
