b22177ede627d2f8efeedf51cf81930489cbcaf3
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-03-11 13:33:42 -0400

    Are arrow functions odd?

---
+++ python/are_arrow_functions_odd.py
@@ -0,0 +1,10 @@
+'''
+DESCRIPTION:
+
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+'''
+
+odds = lambda nums: [x for x in nums if x % 2]
