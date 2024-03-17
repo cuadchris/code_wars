b2809b5e58e70d3ee98846f6813c836bcf883f23
cuadchris <cuadradochris@hotmail.com>
2024-03-17 12:11:59 -0400

    Update array_madness.py

--- python/array_madness.py
+++ python/array_madness.py
@@ -1,16 +1,18 @@
-# https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/python
+'''
+https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/python
 
-# DESCRIPTION:
-# SpeedCode #2 - Array Madness
-# Objective
-# Given two integer arrays a, b, both of length >= 1, 
-# create a program that returns true if the sum of the squares of each 
-# element in a is strictly greater than the sum of the cubes of each element in b.
+DESCRIPTION:
+SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, 
+create a program that returns true if the sum of the squares of each 
+element in a is strictly greater than the sum of the cubes of each element in b.
 
-# E.g.
+E.g.
 
-# array_madness([4, 5, 6], [1, 2, 3]) => True because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
-# Get your timer out. Are you ready? Ready, get set, GO!!!
+array_madness([4, 5, 6], [1, 2, 3]) => True because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+Get your timer out. Are you ready? Ready, get set, GO!!!
+'''
 
 def array_madness(a,b):
     return sum(x**2 for x in a) > sum(x**3 for x in b)
