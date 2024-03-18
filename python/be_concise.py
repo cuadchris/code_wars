a585439fcd2128d793d9f3f038aef5358aa65665
cuadchris <cuadradochris@hotmail.com>
2024-03-18 11:36:09 -0400

    Update be_concise.py

--- python/be_concise.py
+++ python/be_concise.py
@@ -1,12 +1,14 @@
-# https://www.codewars.com/kata/5703c093022cd1aae90012c9/python
+'''
+https://www.codewars.com/kata/5703c093022cd1aae90012c9/python
 
-# DESCRIPTION:
-# Be Concise IV - Index of an element in an array
-# Task
-# Provided is a function find which accepts two parameters in the following order: 
-# array, element and returns the index of the element if found and "Not found" otherwise. 
-# Your task is to shorten the code as much as possible in order to meet the strict character 
-# count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+DESCRIPTION:
+Be Concise IV - Index of an element in an array
+Task
+Provided is a function find which accepts two parameters in the following order: 
+array, element and returns the index of the element if found and "Not found" otherwise. 
+Your task is to shorten the code as much as possible in order to meet the strict character 
+count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+'''
 
 def find(arr, e):
     try: return arr.index(e)
