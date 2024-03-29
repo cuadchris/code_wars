cb9e7c1d43223c011d8c9c5e371a060983eb8642
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-03-29 11:19:51 -0400

    update

--- python/multiplication_table2.py
+++ python/multiplication_table2.py
@@ -1,16 +1,18 @@
-# https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/python
+'''
+https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/python
 
-# DESCRIPTION:
-# Your task, is to create N×N multiplication table, of size provided in parameter.
+DESCRIPTION:
+Your task, is to create N×N multiplication table, of size provided in parameter.
 
-# For example, when given size is 3:
+For example, when given size is 3:
 
-# 1 2 3
-# 2 4 6
-# 3 6 9
-# For the given example, the return value should be:
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
 
-# [[1,2,3],[2,4,6],[3,6,9]]
+[[1,2,3],[2,4,6],[3,6,9]]
+'''
 
 # initial solution
 
