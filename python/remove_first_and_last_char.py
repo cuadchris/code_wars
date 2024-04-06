f3c8360dc9efae0b63bf4cde33e5eabed716c60c
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-04-06 15:34:14 -0400

    update

--- python/remove_first_and_last_char.py
+++ python/remove_first_and_last_char.py
@@ -1,26 +1,28 @@
-# https://www.codewars.com/kata/570597e258b58f6edc00230d/python
+'''
+https://www.codewars.com/kata/570597e258b58f6edc00230d/python
 
-# DESCRIPTION:
+DESCRIPTION:
 
-# This is a spin off of my first kata.
+This is a spin off of my first kata.
 
-# You are given a string containing a sequence of character sequences separated by commas.
+You are given a string containing a sequence of character sequences separated by commas.
 
-# Write a function which returns a new string containing the same character sequences 
-# except the first and the last ones but this time separated by spaces.
+Write a function which returns a new string containing the same character sequences 
+except the first and the last ones but this time separated by spaces.
 
-# If the input string is empty or the removal of the first and last items would cause the 
-# resulting string to be empty, return an empty value (represented as a generic value 
-# NULL in the examples below).
+If the input string is empty or the removal of the first and last items would cause the 
+resulting string to be empty, return an empty value (represented as a generic value 
+NULL in the examples below).
 
-# Examples:
-# "1,2,3"      =>  "2"
-# "1,2,3,4"    =>  "2 3"
-# "1,2,3,4,5"  =>  "2 3 4"
+Examples:
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
 
-# ""     =>  NULL
-# "1"    =>  NULL
-# "1,2"  =>  NULL
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+'''
 
 def array(string):
     return ' '.join(string.split(',')[1:-1]) or None
