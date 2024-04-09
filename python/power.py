b4fb674380650c97848d3cc446e40d89c46c3752
cuadchris <cuadradochris@hotmail.com>
2024-04-09 17:15:11 -0400

    Update power.py

--- python/power.py
+++ python/power.py
@@ -1,14 +1,16 @@
-# https://www.codewars.com/kata/562926c855ca9fdc4800005b/discuss/python
+'''
+https://www.codewars.com/kata/562926c855ca9fdc4800005b/discuss/python
 
-# DESCRIPTION:
-# The goal is to create a function of two inputs number and power, that "raises" the number up to 
-# power (ie multiplies number by itself power times).
+DESCRIPTION:
+The goal is to create a function of two inputs number and power, that "raises" the number up to 
+power (ie multiplies number by itself power times).
 
-# Examples
-# number_to_pwr(3, 2)  # -> 9 ( = 3 * 3 )
-# number_to_pwr(2, 3)  # -> 8 ( = 2 * 2 * 2 )
-# number_to_pwr(10, 6) # -> 1000000
-# Note: math.pow and some others math functions are disabled on final tests.
+Examples
+number_to_pwr(3, 2)  # -> 9 ( = 3 * 3 )
+number_to_pwr(2, 3)  # -> 8 ( = 2 * 2 * 2 )
+number_to_pwr(10, 6) # -> 1000000
+Note: math.pow and some others math functions are disabled on final tests.
+'''
 
 def number_to_pwr(number, p):
     
