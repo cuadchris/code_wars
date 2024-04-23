05510078936f994b8cd8b0516979f4fa882d3db2
cuadchris <cuadradochris@hotmail.com>
2024-04-23 12:18:19 -0400

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- LeetCode/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ LeetCode/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,33 +1,35 @@
-# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=daily-question&envId=2023-10-09
+'''
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=daily-question&envId=2023-10-09
 
-# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
-# If target is not found in the array, return [-1, -1].
+If target is not found in the array, return [-1, -1].
 
-# You must write an algorithm with O(log n) runtime complexity.
+You must write an algorithm with O(log n) runtime complexity.
 
  
 
-# Example 1:
+Example 1:
 
-# Input: nums = [5,7,7,8,8,10], target = 8
-# Output: [3,4]
-# Example 2:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
 
-# Input: nums = [5,7,7,8,8,10], target = 6
-# Output: [-1,-1]
-# Example 3:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
 
-# Input: nums = [], target = 0
-# Output: [-1,-1]
+Input: nums = [], target = 0
+Output: [-1,-1]
  
 
-# Constraints:
+Constraints:
 
-# 0 <= nums.length <= 105
-# -109 <= nums[i] <= 109
-# nums is a non-decreasing array.
-# -109 <= target <= 109
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+'''
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
