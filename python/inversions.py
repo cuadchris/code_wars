f4b11d75862c7be3828b5c429a0eeaec159a04d8
cuadchris <cuadradochris@hotmail.com>
2024-04-28 11:36:13 -0400

    Update inversions.py

--- python/inversions.py
+++ python/inversions.py
@@ -1,17 +1,19 @@
-# https://www.codewars.com/kata/537529f42993de0e0b00181f
+'''
+https://www.codewars.com/kata/537529f42993de0e0b00181f
 
-# DESCRIPTION:
-# Array inversion indicates how far the array is from being sorted.
+DESCRIPTION:
+Array inversion indicates how far the array is from being sorted.
 
-# Inversions are pairs of elements in array that are out of order.
+Inversions are pairs of elements in array that are out of order.
 
-# Examples
-# [1, 2, 3, 4]  =>  0 inversions
-# [1, 3, 2, 4]  =>  1 inversion: 2 and 3
-# [4, 1, 2, 3]  =>  3 inversions: 4 and 1, 4 and 2, 4 and 3
-# [4, 3, 2, 1]  =>  6 inversions: 4 and 3, 4 and 2, 4 and 1, 3 and 2, 3 and 1, 2 and 1
-# Goal
-# The goal is to come up with a function that can calculate inversions for any arbitrary array
+Examples
+[1, 2, 3, 4]  =>  0 inversions
+[1, 3, 2, 4]  =>  1 inversion: 2 and 3
+[4, 1, 2, 3]  =>  3 inversions: 4 and 1, 4 and 2, 4 and 3
+[4, 3, 2, 1]  =>  6 inversions: 4 and 3, 4 and 2, 4 and 1, 3 and 2, 3 and 1, 2 and 1
+Goal
+The goal is to come up with a function that can calculate inversions for any arbitrary array
+'''
 
 def count_inversions(array):
     
