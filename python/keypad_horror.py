c40b5a27f5b5b3c32385029a5af235b8b6e2b2e0
cuadchris <cuadradochris@hotmail.com>
2024-05-01 14:35:28 -0400

    Update keypad_horror.py

--- python/keypad_horror.py
+++ python/keypad_horror.py
@@ -1,27 +1,28 @@
-# https://www.codewars.com/kata/5572392fee5b0180480001ae
+'''
+https://www.codewars.com/kata/5572392fee5b0180480001ae
 
-# DESCRIPTION:
-# Having two standards for a keypad layout is inconvenient!
-# Computer keypad's layout:
-# 7 8 9  \n
-# 4 5 6  \n
-# 1 2 3  \n
-#   0 \n
+DESCRIPTION:
+Having two standards for a keypad layout is inconvenient!
+Computer keypad's layout:
+7 8 9  \n
+4 5 6  \n
+1 2 3  \n
+  0 \n
 
-# Cell phone keypad's layout:
-# 1 2 3\n 
-# 4 5 6\n  
-# 7 8 9\n  
-#   0\n
+Cell phone keypad's layout:
+1 2 3\n 
+4 5 6\n  
+7 8 9\n  
+  0\n
 
-# Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
+Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
 
-# Example:
-# "789" -> "123"
+Example:
+"789" -> "123"
 
-# Notes:
-# You get a string with numbers only
-
+Notes:
+You get a string with numbers only
+'''
 
 # Cool 1-liner
 def computer_to_phone(numbers):
