6ad8c8bc6a18010c9bf71e77f6858d9e4e18c65a
cuadchris <cuadradochris@hotmail.com>
2024-05-04 09:15:37 -0400

    Update kata_title_too_long.py

--- python/kata_title_too_long.py
+++ python/kata_title_too_long.py
@@ -1,20 +1,22 @@
-# https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7/python
+'''
+https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7/python
 
-# DESCRIPTION:
-# Task
-# Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result 
-# should be a boolean value, instead of a string.
+DESCRIPTION:
+Task
+Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result 
+should be a boolean value, instead of a string.
 
-# The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that 
-# the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are 
-# empty then you should return false/False.
+The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that 
+the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are 
+empty then you should return false/False.
 
-# Examples (input -> output)
-# "ab","AB"     -> true
-# "aB","Ab"     -> true
-# "aBcd","AbCD" -> true
-# "AB","Ab"     -> false
-# "",""         -> false
+Examples (input -> output)
+"ab","AB"     -> true
+"aB","Ab"     -> true
+"aBcd","AbCD" -> true
+"AB","Ab"     -> false
+"",""         -> false
+'''
 
 def is_opposite(s1,s2):
     
