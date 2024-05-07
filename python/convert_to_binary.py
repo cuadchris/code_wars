6fd7ea84c659c0d786b8c68dffc83757186bc1ce
cuadchris <cuadradochris@hotmail.com>
2024-05-07 09:39:30 -0400

    Update convert_to_binary.py

--- python/convert_to_binary.py
+++ python/convert_to_binary.py
@@ -1,13 +1,15 @@
-# https://www.codewars.com/kata/59fca81a5712f9fa4700159a/python
+'''
+https://www.codewars.com/kata/59fca81a5712f9fa4700159a/python
 
-# DESCRIPTION:
-# Task Overview
-# Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in 
-# a binary format.
+DESCRIPTION:
+Task Overview
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in 
+a binary format.
 
-# to_binary(1)   should return 1 
-# to_binary(5)   should return 101
-# to_binary(11)  should return 1011
+to_binary(1)   should return 1 
+to_binary(5)   should return 101
+to_binary(11)  should return 1011
+'''
 
 def to_binary(n, res = ''):
 
