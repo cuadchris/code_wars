9d313edb8b4ddde2006aa9cf6fe0235f80b6ea9e
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-05-12 11:41:17 -0400

    Working with arrays

---
+++ python/working_with_arrays_I_and_why_your_code_fails_in_some_katas.py
@@ -0,0 +1,23 @@
+'''
+DESCRIPTION:
+In this kata the function returns an array/list of numbers without its last element. The function is
+already written for you and the basic tests pass, but random tests fail. Your task is to figure out why
+and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+Some good reading: Python Lists
+'''
+
+# initial code to be fixed.
+def without_last(lst):
+    # Fix it
+    lst.pop() # removes the last element
+    return lst
+
+# solution. The lesson: don't mutate the input.
+def without_last(lst):
+    
+    return lst[:-1]
