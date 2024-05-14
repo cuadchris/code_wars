8f9d0d9af75c22fe7091516632b59a8d4d2b8506
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-05-14 11:17:56 -0400

    link

--- python/difference_between_two_collections.py
+++ python/difference_between_two_collections.py
@@ -1,4 +1,6 @@
 '''
+https://www.codewars.com/kata/594093784aafb857f0000122
+
 DESCRIPTION:
 Find the difference between two collections. The difference means that either the character is present in
 one collection or it is present in other, but not in both. Return a sorted list with the difference.
