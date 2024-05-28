5c0f6853853e588fe59407dc88f3efe91fec6271
cuadchris <cuadradochris@hotmail.com>
2024-05-28 10:09:01 -0400

    Update day_of_the_year.js

--- python/day_of_the_year.js
+++ python/day_of_the_year.js
@@ -1,19 +1,21 @@
-// https://www.codewars.com/kata/5a1ebe0d46d843454100004c/javascript
+/**
+https://www.codewars.com/kata/5a1ebe0d46d843454100004c/javascript
 
-// DESCRIPTION:
-// Work out what number day of the year it is.
+DESCRIPTION:
+Work out what number day of the year it is.
 
-// toDayOfYear([1, 1, 2000]) => 1
+toDayOfYear([1, 1, 2000]) => 1
 
-// The argument passed into the function is an array with the format [D, M, YYYY], e.g. [1, 2, 2000] 
-// for February 1st, 2000 or [22, 12, 1999] for December 22nd, 1999.
+The argument passed into the function is an array with the format [D, M, YYYY], e.g. [1, 2, 2000] 
+for February 1st, 2000 or [22, 12, 1999] for December 22nd, 1999.
 
-// Don't forget to check for whether it's a leap year! Three criteria must be taken into account to identify 
-// leap years:
+Don't forget to check for whether it's a leap year! Three criteria must be taken into account to identify 
+leap years:
 
-// The year can be evenly divided by 4;
-// If the year can be evenly divided by 100, it is NOT a leap year, unless;
-// The year is also evenly divisible by 400. Then it is a leap year.
+The year can be evenly divided by 4;
+If the year can be evenly divided by 100, it is NOT a leap year, unless;
+The year is also evenly divisible by 400. Then it is a leap year.
+*/
 
 function toDayOfYear(date) {
     const [day, month, year] = date;
