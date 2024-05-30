d4dbd4ead9afb18c555107bbe6b62932280b2d06
cuadchris <cuadradochris@hotmail.com>
2024-05-30 07:50:01 -0400

    Update green_glass_door.py

--- python/green_glass_door.py
+++ python/green_glass_door.py
@@ -1,21 +1,23 @@
-# https://www.codewars.com/kata/5642bf07a586135a6f000004/python
+'''
+https://www.codewars.com/kata/5642bf07a586135a6f000004/python
 
-# DESCRIPTION:
-# Step through my green glass door.
+DESCRIPTION:
+Step through my green glass door.
 
-# You can take the moon, but not the sun.
+You can take the moon, but not the sun.
 
-# You can take your slippers, but not your sandals.
+You can take your slippers, but not your sandals.
 
-# You can go through yelling, but not shouting.
+You can go through yelling, but not shouting.
 
-# You can't run through fast, but you can run with speed.
+You can't run through fast, but you can run with speed.
 
-# You can take a sheet, but not your blanket.
+You can take a sheet, but not your blanket.
 
-# You can wear your glasses, but not your contacts.
+You can wear your glasses, but not your contacts.
 
-# Have you figured it out? Good! Then write a program that can figure it out as well.
+Have you figured it out? Good! Then write a program that can figure it out as well.
+'''
 
 def step_through_with(s):
     
