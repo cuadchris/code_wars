d01aef66d1e45a2a32293de6e220a27da967a43b
cuadchris <cuadradochris@hotmail.com>
2024-06-09 11:33:31 -0400

    Update simple_pig_latin.py

--- python/simple_pig_latin.py
+++ python/simple_pig_latin.py
@@ -1,13 +1,15 @@
-# https://www.codewars.com/kata/520b9d2ad5c005041100000f
+'''
+https://www.codewars.com/kata/520b9d2ad5c005041100000f
 
-# DESCRIPTION:
-# 5kyu
-# Move the first letter of each word to the end of it, then add "ay" to the end
-#  of the word. Leave punctuation marks untouched.
+DESCRIPTION:
+5kyu
+Move the first letter of each word to the end of it, then add "ay" to the end
+of the word. Leave punctuation marks untouched.
 
-# Examples
-# pig_it('Pig latin is cool') igPay atinlay siay oolcay
-# pig_it('Hello world !')     elloHay orldway !
+Examples
+pig_it('Pig latin is cool') igPay atinlay siay oolcay
+pig_it('Hello world !')     elloHay orldway !
+'''
 
 def pig_it(text):
     return ' '.join(f'{c[1:]}{c[0]}ay' if c.isalpha() else c for c in text.split())
