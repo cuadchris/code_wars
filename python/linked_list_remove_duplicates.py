e38084bc888e4364d1c211fa07e4d0ce16bf9a30
cuadchris <cuadradochris@hotmail.com>
2024-06-11 11:59:40 -0400

    Update linked_list_remove_duplicates.py

--- python/linked_list_remove_duplicates.py
+++ python/linked_list_remove_duplicates.py
@@ -1,20 +1,22 @@
-# https://www.codewars.com/kata/55d9f257d60c5fd98d00001b/python
+'''
+https://www.codewars.com/kata/55d9f257d60c5fd98d00001b/python
 
-# DESCRIPTION:
+DESCRIPTION:
 
-# Linked Lists - Remove Duplicates
+Linked Lists - Remove Duplicates
 
-# Write a RemoveDuplicates() function which takes a list sorted in increasing order and 
-# deletes any duplicate nodes from the list. Ideally, the list should only be traversed 
-# once. The head of the resulting list should be returned.
+Write a RemoveDuplicates() function which takes a list sorted in increasing order and 
+deletes any duplicate nodes from the list. Ideally, the list should only be traversed 
+once. The head of the resulting list should be returned.
 
-# var list = 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5 -> null
-# removeDuplicates(list) === 1 -> 2 -> 3 -> 4 -> 5 -> null
-# If the passed in list is null/None/nil, simply return null.
+var list = 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5 -> null
+removeDuplicates(list) === 1 -> 2 -> 3 -> 4 -> 5 -> null
+If the passed in list is null/None/nil, simply return null.
 
-# Note: Your solution is expected to work on long lists. Recursive solutions may fail due 
-# to stack size limitations.
-
+Note: Your solution is expected to work on long lists. Recursive solutions may fail due 
+to stack size limitations.
+'''
+    
 class Node(object):
     def __init__(self, data):
         self.data = data
