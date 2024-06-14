0e677d0a699992229ea0a4045fcc617505b448ea
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-06-14 11:38:57 -0400

    Grasshopper - Function syntax debugging

---
+++ python/grasshopper_function_syntax_debugging.py
@@ -0,0 +1,19 @@
+'''
+DESCRIPTION:
+
+Grasshopper - Function syntax debugging
+
+A student was working on a function and made some syntax mistakes while coding. Help them find their
+mistakes and fix them.
+'''
+
+# Code to fix:
+
+# def main [verb, noun]
+# return verb + noun
+
+# Have fun with it, lol.
+res = lambda x, y: x + y
+
+def main(verb, noun):
+    return res(verb, noun)
