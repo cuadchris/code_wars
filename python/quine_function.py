f331bd7c96b58d679ce9f8f52f04e4632eecae04
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-06-21 11:38:17 -0400

    Quine function

---
+++ python/quine_function.py
@@ -0,0 +1,12 @@
+'''
+DESCRIPTION:
+
+Quine is a nonempty program that prints itself. Your task is bit different than that. Write a function
+that takes no parameters and returns your program as a string instead of printing it.
+'''
+
+def quine():
+    source_code = 'def quine():\n    source_code = {!r}\n    return source_code.format(source_code)'
+    return source_code.format(source_code)
+
+# Lots of other interesting solutions for this.
