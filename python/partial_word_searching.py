59d796f5b6e55863bc144c84076225f5a8a22c9d
cuadchris <cuadradochris@hotmail.com>
2024-06-26 15:15:21 -0400

    Update partial_word_searching.py

--- python/partial_word_searching.py
+++ python/partial_word_searching.py
@@ -1,4 +1,6 @@
 '''
+https://www.codewars.com/kata/54b81566cd7f51408300022d
+
 DESCRIPTION:
 Write a method that will search an array of strings for all strings that contain another string, ignoring
 capitalization. Then return an array of the found strings.
