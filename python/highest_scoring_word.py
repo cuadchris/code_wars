a6081b277aa2cf43ee8d5f41232940e4ec6917c5
cuadchris <cuadradochris@hotmail.com>
2024-06-30 08:46:31 -0400

    Update highest_scoring_word.py

--- python/highest_scoring_word.py
+++ python/highest_scoring_word.py
@@ -1,17 +1,19 @@
-# https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
+'''
+https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
 
-# DESCRIPTION:
-# Given a string of words, you need to find the highest scoring word.
+DESCRIPTION:
+Given a string of words, you need to find the highest scoring word.
 
-# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
 
-# For example, the score of abad is 8 (1 + 2 + 1 + 4).
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
 
-# You need to return the highest scoring word as a string.
+You need to return the highest scoring word as a string.
 
-# If two words score the same, return the word that appears earliest in the original string.
+If two words score the same, return the word that appears earliest in the original string.
 
-# All letters will be lowercase and all inputs will be valid.
+All letters will be lowercase and all inputs will be valid.
+'''
 
 def high(x):
     
