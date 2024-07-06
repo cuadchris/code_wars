f664420c7a8cd34972b0d088656a0ba3abae5692
cuadchris <cuadradochris@hotmail.com>
2024-07-06 11:30:55 -0400

    Update length_and_two_values.py

--- python/length_and_two_values.py
+++ python/length_and_two_values.py
@@ -1,14 +1,16 @@
-# https://www.codewars.com/kata/62a611067274990047f431a8/python
+'''
+https://www.codewars.com/kata/62a611067274990047f431a8/python
 
-# DESCRIPTION:
-# Given an integer n and two other values, build an array of size n filled with these two values 
-# alternating.
+DESCRIPTION:
+Given an integer n and two other values, build an array of size n filled with these two values 
+alternating.
 
-# Examples
-# 5, true, false     -->  [true, false, true, false, true]
-# 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
-# 0, "one", "two"    -->  []
-# Good luck!
+Examples
+5, true, false     -->  [true, false, true, false, true]
+10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+0, "one", "two"    -->  []
+Good luck!
+'''
 
 def alternate(n, first_value, second_value):
     return [first_value if not i % 2 else second_value for i in range(n)]
