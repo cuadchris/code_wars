0e1e4507219db951eac24022d8f82fe4b2f74303
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-07-11 10:57:41 -0400

    3 powers of 2

---
+++ python/3_powers_of_2.py
@@ -0,0 +1,21 @@
+'''
+DESCRIPTION:
+
+Write a function three_powers() to accept a number, to check can it represent as sum of 3 powers of 2.
+(n == 2**i + 2**j + 2**k, i, j, k >= 0)
+
+For example:
+
+three_powers(2)  # False
+three_powers(3)  # True, 3 = 2**0 + 2**0 + 2**0
+three_powers(5)  # True, 5 = 2**0 + 2**1 + 2**1
+three_powers(15)  # False
+
+Input:
+- `n` must be an integer.
+- `1` <= `n` <= `2 ** 100 - 1`
+There are 2000 random tests for the speed test, good luck and happy coding.
+'''
+
+def three_powers(n):
+    return n > 2 and n.bit_count() <= 3
