5bc36231242b345e3d547d4298110389090bd95f
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-07-16 11:33:54 -0400

    Recaman Sequence

---
+++ python/recaman_sequence.py
@@ -0,0 +1,33 @@
+'''
+DESCRIPTION:
+
+Return the nth term of the Recamán's sequence.
+
+a(0) = 0;
+
+        a(n-1) - n, if this value is positive and not yet in the sequence
+      /
+a(n) <
+      \
+        a(n-1) + n, otherwise
+
+input range: 0 – 30 000
+'''
+
+def recaman(n):
+    
+    if n == 0: return 0
+    
+    sequence = [0] * (n + 1)
+    used = set([0])
+    
+    for i in range(1, n + 1):
+        candidate = sequence[i - 1] - i
+        if candidate > 0 and candidate not in used:
+            sequence[i] = candidate
+        else:
+            candidate = sequence[i - 1] + i
+            sequence[i] = candidate
+        used.add(candidate)
+    
+    return sequence[n]
