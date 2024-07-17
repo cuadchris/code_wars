8c2982ad78f2c29a73a752949ca36ee6588ea025
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-07-17 11:31:22 -0400

    Number Pairs

---
+++ python/number_pairs.py
@@ -0,0 +1,17 @@
+'''
+DESCRIPTION:
+
+In this kata the aim is to compare each pair of integers from two arrays, and return a new array of large
+numbers.
+
+Note: Both arrays have the same dimensions.
+
+Example:
+arr1 = [13, 64, 15, 17, 88]
+arr2 = [23, 14, 53, 17, 80]
+get_larger_numbers(arr1, arr2) == [23, 64, 53, 17, 88]
+'''
+
+get_larger_numbers = lambda a, b: [max(x, y) for x, y in zip(a, b)]
+
+# lambdas are cool, as is the zip function.
