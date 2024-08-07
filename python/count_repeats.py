ab8e92f1e4767885e8e95654dda4eb255406620a
Christopher Cuadrado <cuadradochris@hotmail.com>
2024-08-07 12:58:23 -0400

    Count Repeats

---
+++ python/count_repeats.py
@@ -0,0 +1,17 @@
+'''
+Description:
+
+Write a function that returns the count of characters that have to be removed in order to get a string
+with no consecutive repeats.
+
+Note: This includes any characters
+
+Examples:
+'abbbbc'  => 'abc'    #  answer: 3
+'abbcca'  => 'abca'   #  answer: 2
+'ab cca'  => 'ab ca'  #  answer: 1
+'''
+
+def count_repeats(txt):
+    
+    return sum(1 for a, b in zip(txt, txt[1:]) if a == b)
