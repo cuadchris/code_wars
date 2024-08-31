300aa307c5d45d076c4ae7efceb913d15e5224f3
cuadchris <cuadradochris@hotmail.com>
2024-08-31 07:43:00 -0400

    Update count IP_addresses.py

--- python/count IP_addresses.py
+++ python/count IP_addresses.py
@@ -1,4 +1,6 @@
 '''
+https://www.codewars.com/kata/526989a41034285187000de4
+
 DESCRIPTION:
 Implement a function that receives two IPv4 addresses, and returns the number of addresses between them
 (including the first one, excluding the last one).
