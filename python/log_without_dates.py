61ed03444268786b7a3f9dfc1c381b7cb7baed9a
cuadchris <cuadradochris@hotmail.com>
2024-09-13 07:12:36 -0400

    Update log_without_dates.py

--- python/log_without_dates.py
+++ python/log_without_dates.py
@@ -1,28 +1,30 @@
-# https://www.codewars.com/kata/64cac86333ab6a14f70c6fb6/python
+'''
+https://www.codewars.com/kata/64cac86333ab6a14f70c6fb6/python
 
-# DESCRIPTION:
-# You will be given an array of events, which are represented by strings. The strings are 
-# dates in HH:MM:SS format.
+DESCRIPTION:
+You will be given an array of events, which are represented by strings. The strings are 
+dates in HH:MM:SS format.
 
-# It is known that all events are recorded in chronological order and two events can't 
-# occur in the same second.
+It is known that all events are recorded in chronological order and two events can't 
+occur in the same second.
 
-# Return the minimum number of days during which the log is written.
+Return the minimum number of days during which the log is written.
 
-# Example:
+Example:
 
-# Input -> ["00:00:00", "00:01:11", "02:15:59", "23:59:58", "23:59:59"]
-# Output -> 1
+Input -> ["00:00:00", "00:01:11", "02:15:59", "23:59:58", "23:59:59"]
+Output -> 1
 
-# Input -> ["12:12:12"]
-# Output -> 1
+Input -> ["12:12:12"]
+Output -> 1
 
-# Input -> ["12:00:00", "23:59:59", "00:00:00"]
-# Output -> 2
+Input -> ["12:00:00", "23:59:59", "00:00:00"]
+Output -> 2
 
-# Input -> []
-# Output -> 0
-# Good luck
+Input -> []
+Output -> 0
+Good luck
+'''
 
 def check_logs(log):
 
