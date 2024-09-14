d941a0ce6c365fadce58cbc056c07ee106617df5
cuadchris <cuadradochris@hotmail.com>
2024-09-14 08:27:53 -0400

    Update max_sub_array.py

--- python/max_sub_array.py
+++ python/max_sub_array.py
@@ -1,16 +1,18 @@
-# https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/python
+'''
+https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/python
 
-# DESCRIPTION:
-# The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence 
-# in an array or list of integers:
+DESCRIPTION:
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence 
+in an array or list of integers:
 
-# max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-# # should be 6: [4, -1, 2, 1]
-# Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of 
-# the whole array. If the list is made up of only negative numbers, return 0 instead.
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of 
+the whole array. If the list is made up of only negative numbers, return 0 instead.
 
-# Empty list is considered to have zero greatest sum. Note that the empty list or array is also a 
-# valid sublist/subarray.
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a 
+valid sublist/subarray.
+'''
 
 def max_sequence(arr):
     
