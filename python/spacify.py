46d44312984a83a0200769941e4098daca951d12
cuadchris <cuadradochris@hotmail.com>
2024-09-15 08:26:09 -0400

    Update spacify.py

--- python/spacify.py
+++ python/spacify.py
@@ -1,10 +1,12 @@
-# https://www.codewars.com/kata/57f8ee485cae443c4d000127
+'''
+https://www.codewars.com/kata/57f8ee485cae443c4d000127
 
-# DESCRIPTION:
-# Modify the spacify function so that it returns the given string with spaces inserted between
-# each character.
+DESCRIPTION:
+Modify the spacify function so that it returns the given string with spaces inserted between
+each character.
 
-# spacify("hello world") # returns "h e l l o   w o r l d"
+spacify("hello world") # returns "h e l l o   w o r l d"
+'''
 
 def spacify(string):
     return " ".join(string)
