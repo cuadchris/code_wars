94b57a4172cc2d8e2650c1b2c00ca7206735ef12
cuadchris <cuadradochris@hotmail.com>
2024-09-20 15:42:23 -0400

    Update sum_factorial.py

--- python/sum_factorial.py
+++ python/sum_factorial.py
@@ -1,27 +1,29 @@
-# https://www.codewars.com/kata/56b0f6243196b9d42d000034/python
+'''
+https://www.codewars.com/kata/56b0f6243196b9d42d000034/python
 
-# DESCRIPTION:
-# Factorials are often used in probability and are used as an introductory problem for looping 
-# constructs. In this kata you will be summing together multiple factorials.
+DESCRIPTION:
+Factorials are often used in probability and are used as an introductory problem for looping 
+constructs. In this kata you will be summing together multiple factorials.
 
-# Here are a few examples of factorials:
+Here are a few examples of factorials:
 
-# 4 Factorial = 4! = 4 * 3 * 2 * 1 = 24
+4 Factorial = 4! = 4 * 3 * 2 * 1 = 24
 
-# 6 Factorial = 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
-# In this kata you will be given a list of values that you must first find the factorial, 
-# and then return their sum.
+6 Factorial = 6! = 6 * 5 * 4 * 3 * 2 * 1 = 720
+In this kata you will be given a list of values that you must first find the factorial, 
+and then return their sum.
 
-# For example if you are passed the list [4, 6] the equivalent mathematical expression would be 
-# 4! + 6! which would equal 744.
+For example if you are passed the list [4, 6] the equivalent mathematical expression would be 
+4! + 6! which would equal 744.
 
-# Good Luck!
+Good Luck!
 
-# Note: Assume that all values in the list are positive integer values > 0 and each value in the 
-# list is unique.
+Note: Assume that all values in the list are positive integer values > 0 and each value in the 
+list is unique.
 
-# Also, you must write your own implementation of factorial, as you cannot use the built-in 
-# math.factorial() method.
+Also, you must write your own implementation of factorial, as you cannot use the built-in 
+math.factorial() method.
+'''
 
 def sum_factorial(lst):
         
