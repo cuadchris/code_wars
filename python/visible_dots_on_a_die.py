b8bf06ff3a3135cebd841d8ae7b287359ce451f0
cuadchris <cuadradochris@hotmail.com>
2024-09-30 08:00:25 -0400

    Update visible_dots_on_a_die.py

--- python/visible_dots_on_a_die.py
+++ python/visible_dots_on_a_die.py
@@ -1,20 +1,22 @@
-# https://www.codewars.com/kata/5a39724945ddce2223000800/python
+'''
+https://www.codewars.com/kata/5a39724945ddce2223000800/python
 
-# DESCRIPTION:
+DESCRIPTION:
 
-# Your task is to return the number of visible dots on a die after it has been rolled
-# (that means the total count of dots that would not be touching the table when rolled)
+Your task is to return the number of visible dots on a die after it has been rolled
+(that means the total count of dots that would not be touching the table when rolled)
 
-# 6, 8, 10, 12, 20 sided dice are the possible inputs for "numOfSides"
+6, 8, 10, 12, 20 sided dice are the possible inputs for "numOfSides"
 
-# topNum is equal to the number that is on top, or the number that was rolled.
+topNum is equal to the number that is on top, or the number that was rolled.
 
-# for this exercise, all opposite faces add up to be 1 more than the total amount of sides 
+for this exercise, all opposite faces add up to be 1 more than the total amount of sides 
 
-# Example: 6 sided die would have 6 opposite 1, 4 opposite 3, and so on. for this exercise, the 10 sided die 
-# starts at 1 and ends on 10.
+Example: 6 sided die would have 6 opposite 1, 4 opposite 3, and so on. for this exercise, the 10 sided die 
+starts at 1 and ends on 10.
 
-# Note: topNum will never be greater than numOfSides
+Note: topNum will never be greater than numOfSides
+'''
 
 def totalAmountVisible(topNum, numOfSides):
     
