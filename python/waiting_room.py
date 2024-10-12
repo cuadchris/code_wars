c1f0144bdfd97f4f4ee9c1773994965828825e92
cuadchris <cuadradochris@hotmail.com>
2024-10-12 14:59:16 -0400

    Update waiting_room.py

--- python/waiting_room.py
+++ python/waiting_room.py
@@ -1,26 +1,28 @@
-# https://www.codewars.com/kata/542f0c36d002f8cd8a0005e5/python
+'''
+https://www.codewars.com/kata/542f0c36d002f8cd8a0005e5/python
 
-# DESCRIPTION:
-# There's a waiting room with N chairs set in single row. Chairs are consecutively numbered from 1 to N. 
-# First is closest to the entrance (which is exit as well).
+DESCRIPTION:
+There's a waiting room with N chairs set in single row. Chairs are consecutively numbered from 1 to N. 
+First is closest to the entrance (which is exit as well).
 
-# For some reason people choose a chair in the following way
+For some reason people choose a chair in the following way
 
-# Find a place as far from other people as possible
-# Find a place as close to exit as possible
-# All chairs must be occupied before the first person will be served
+Find a place as far from other people as possible
+Find a place as close to exit as possible
+All chairs must be occupied before the first person will be served
 
-# So it looks like this for 10 chairs and 10 patients
+So it looks like this for 10 chairs and 10 patients
 
-# Chairs	1	2	3	4	5	6	7	8	9	10
-# Patients	1	7	5	8	3	9	4	6	10	2
+Chairs	1	2	3	4	5	6	7	8	9	10
+Patients	1	7	5	8	3	9	4	6	10	2
 
-# Your task is to find last patient's chair's number.
+Your task is to find last patient's chair's number.
 
-# Input: number of chairs N, an integer greater than 2.
-# Output: a positive integer, the last patient's chair number.
+Input: number of chairs N, an integer greater than 2.
+Output: a positive integer, the last patient's chair number.
 
-# Have fun :)
+Have fun :)
+'''
 
 def last_chair(n):
     return n - 1
