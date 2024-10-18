d3e8715016709f228dabe44b9ba0de9ee0f50065
cuadchris <cuadradochris@hotmail.com>
2024-10-18 13:49:24 -0400

    Update transpose.py

--- python/transpose.py
+++ python/transpose.py
@@ -1,26 +1,28 @@
-# https://www.codewars.com/kata/581f4ac139dc423f04000b99/python
+'''
+https://www.codewars.com/kata/581f4ac139dc423f04000b99/python
 
-# DESCRIPTION:
+DESCRIPTION:
 
-# You will be given an array that contains two strings. Your job is to create a function 
-# that will take those two strings and transpose them, so that the strings go from top to 
-# bottom instead of left to right.
+You will be given an array that contains two strings. Your job is to create a function 
+that will take those two strings and transpose them, so that the strings go from top to 
+bottom instead of left to right.
 
-# e.g. transposeTwoStrings(['Hello','World']);
+e.g. transposeTwoStrings(['Hello','World']);
 
-# should return
+should return
 
-# H W  
-# e o  
-# l r  
-# l l  
-# o d
-# A few things to note:
+H W  
+e o  
+l r  
+l l  
+o d
+A few things to note:
 
-# There should be one space in between the two characters
-# You don't have to modify the case (i.e. no need to change to upper or lower)
-# If one string is longer than the other, there should be a space where the character 
-# would be
+There should be one space in between the two characters
+You don't have to modify the case (i.e. no need to change to upper or lower)
+If one string is longer than the other, there should be a space where the character 
+would be
+'''
 
 def transpose_two_strings(arr):
     
