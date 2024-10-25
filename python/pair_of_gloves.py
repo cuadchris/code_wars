09b9ddc0ef2048361a9fb6dc02ff18a8fe88d28a
cuadchris <cuadradochris@hotmail.com>
2024-10-25 10:04:31 -0400

    Update pair_of_gloves.py

--- python/pair_of_gloves.py
+++ python/pair_of_gloves.py
@@ -1,19 +1,21 @@
-# https://www.codewars.com/kata/58235a167a8cb37e1a0000db/python
+'''
+https://www.codewars.com/kata/58235a167a8cb37e1a0000db/python
 
-# DESCRIPTION:
-# Pair of gloves
-# Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine 
-# the number of pair of gloves you can constitute from the gloves you have in your drawer.
+DESCRIPTION:
+Pair of gloves
+Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine 
+the number of pair of gloves you can constitute from the gloves you have in your drawer.
 
-# Given an array describing the color of each glove, return the number of pairs you can constitute, 
-# assuming that only gloves of the same color can form pairs.
+Given an array describing the color of each glove, return the number of pairs you can constitute, 
+assuming that only gloves of the same color can form pairs.
 
-# Examples:
-# input = ["red", "green", "red", "blue", "blue"]
-# result = 2 (1 red pair + 1 blue pair)
+Examples:
+input = ["red", "green", "red", "blue", "blue"]
+result = 2 (1 red pair + 1 blue pair)
 
-# input = ["red", "red", "red", "red", "red", "red"]
-# result = 3 (3 red pairs)
+input = ["red", "red", "red", "red", "red", "red"]
+result = 3 (3 red pairs)
+'''
 
 def number_of_pairs(gloves):
     
