d8005e1390ebefc7137d5a3cc1a05041a9105373
cuadchris <cuadradochris@hotmail.com>
2024-10-28 13:24:38 -0400

    Update price_of_mangoes.py

--- python/price_of_mangoes.py
+++ python/price_of_mangoes.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/57a77726bb9944d000000b06/python
 
 DESCRIPTION:
+
 There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), 
 calculate the total cost of the mangoes.
 
