d1f2e8ec69f6efd547938835a82a2d1b07fcd730
cuadchris <cuadradochris@hotmail.com>
2024-10-30 15:56:21 -0400

    Update convert_to_binary.py

--- python/convert_to_binary.py
+++ python/convert_to_binary.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/59fca81a5712f9fa4700159a/python
 
 DESCRIPTION:
+
 Task Overview
 Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in 
 a binary format.
