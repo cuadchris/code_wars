b8ab6b795dd38d1e3296bc9696cde470a4869b3a
cuadchris <cuadradochris@hotmail.com>
2024-11-11 14:00:39 -0500

    Update length_and_two_values.py

--- python/length_and_two_values.py
+++ python/length_and_two_values.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/62a611067274990047f431a8/python
 
 DESCRIPTION:
+
 Given an integer n and two other values, build an array of size n filled with these two values 
 alternating.
 
