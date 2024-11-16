b5e9e778972a581455e63419a40514521a11c77e
cuadchris <cuadradochris@hotmail.com>
2024-11-16 13:23:10 -0500

    Update max_sub_array.py

--- python/max_sub_array.py
+++ python/max_sub_array.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/python
 
 DESCRIPTION:
+
 The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence 
 in an array or list of integers:
 
