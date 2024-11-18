afdca34d0eca9637e7eeb25baeec8721684766cf
cuadchris <cuadradochris@hotmail.com>
2024-11-18 09:19:41 -0500

    Update be_concise.py

--- python/be_concise.py
+++ python/be_concise.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/5703c093022cd1aae90012c9/python
 
 DESCRIPTION:
+
 Be Concise IV - Index of an element in an array
 Task
 Provided is a function find which accepts two parameters in the following order: 
