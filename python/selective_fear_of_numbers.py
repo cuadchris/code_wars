df9c38207421cac140add28d20a13e65443dc248
cuadchris <cuadradochris@hotmail.com>
2024-11-21 13:01:36 -0500

    Update selective_fear_of_numbers.py

--- python/selective_fear_of_numbers.py
+++ python/selective_fear_of_numbers.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/55b1fd84a24ad00b32000075/python
 
 DESCRIPTION:
+
 I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated:
 The number I'm afraid of depends on which day of the week it is... This is a concrete 
 description of my mental illness:
