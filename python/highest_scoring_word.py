6f5c6d064388eafb3b46e7aa22d7ec54ae9f4b79
cuadchris <cuadradochris@hotmail.com>
2024-11-22 11:06:25 -0500

    Update highest_scoring_word.py

--- python/highest_scoring_word.py
+++ python/highest_scoring_word.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
 
 DESCRIPTION:
+
 Given a string of words, you need to find the highest scoring word.
 
 Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
