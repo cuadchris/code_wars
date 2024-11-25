3045632a5a4b166ab2fe4c8aa7017a147c6c3c43
cuadchris <cuadradochris@hotmail.com>
2024-11-25 12:50:43 -0500

    Update drop_while.py

--- python/drop_while.py
+++ python/drop_while.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/54f9c37106098647f400080a/python
 
 DESCRIPTION:
+
 Yet another staple for the functional programmer. You have a sequence of values and some predicate for those 
 values. You want to remove the longest prefix of elements such that the predicate is true for each element. 
 We'll call this the dropWhile function. It accepts two arguments. The first is the sequence of values, and the 
