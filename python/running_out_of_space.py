9c424ed5109bfbfc709d4a794f9a9e09a9f241e8
cuadchris <cuadradochris@hotmail.com>
2024-12-12 11:55:52 -0500

    Update running_out_of_space.py

--- python/running_out_of_space.py
+++ python/running_out_of_space.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/56576f82ab83ee8268000059/python
 
 DESCRIPTION:
+
 Kevin is noticing his space run out! Write a function that removes the spaces from the values 
 and returns an array showing the space decreasing.
 For example, running this function on the array ['i', 'have','no','space'] 
