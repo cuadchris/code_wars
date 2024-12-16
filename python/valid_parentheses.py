5bb658902df7d68fe75885b1ddc0d93dfc045289
cuadchris <cuadradochris@hotmail.com>
2024-12-16 13:39:47 -0500

    Update valid_parentheses.py

--- python/valid_parentheses.py
+++ python/valid_parentheses.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/6411b91a5e71b915d237332d
 
 DESCRIPTION:
+
 Write a function that takes a string of parentheses, and determines if the order of the 
 parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
