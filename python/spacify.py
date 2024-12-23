133721a936ad43dd87b8da08021926f8a4bbb4f4
cuadchris <cuadradochris@hotmail.com>
2024-12-23 11:52:44 -0500

    Update spacify.py

--- python/spacify.py
+++ python/spacify.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/57f8ee485cae443c4d000127
 
 DESCRIPTION:
+
 Modify the spacify function so that it returns the given string with spaces inserted between
 each character.
 
