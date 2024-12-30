e2bbab5b66dbc4e9cf37e1817f776907172909fc
cuadchris <cuadradochris@hotmail.com>
2024-12-30 13:01:01 -0500

    Update simple_pig_latin.py

--- python/simple_pig_latin.py
+++ python/simple_pig_latin.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/520b9d2ad5c005041100000f
 
 DESCRIPTION:
+
 5kyu
 Move the first letter of each word to the end of it, then add "ay" to the end
 of the word. Leave punctuation marks untouched.
