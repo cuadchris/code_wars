3cfa5d7ae6e6e2b98f3da5302b01b652c4e49bed
cuadchris <cuadradochris@hotmail.com>
2025-01-12 09:21:59 -0500

    Update debug_sum_of_digits.py

--- python/debug_sum_of_digits.py
+++ python/debug_sum_of_digits.py
@@ -6,7 +6,8 @@ DESCRIPTION:
 Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. 
 Assume that argument is an integer.
 
-Example
+Example:
+
 123  => 6
 223  => 7
 1337 => 14
