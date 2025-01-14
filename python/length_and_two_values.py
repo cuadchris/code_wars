c9f67b0ae2ea2d6412e6457cba507639460d8798
cuadchris <cuadradochris@hotmail.com>
2025-01-14 10:07:41 -0500

    Update length_and_two_values.py

--- python/length_and_two_values.py
+++ python/length_and_two_values.py
@@ -6,7 +6,8 @@ DESCRIPTION:
 Given an integer n and two other values, build an array of size n filled with these two values 
 alternating.
 
-Examples
+Examples:
+
 5, true, false     -->  [true, false, true, false, true]
 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
 0, "one", "two"    -->  []
