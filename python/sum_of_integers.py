e5af6dbdbcc2c870f6978b1e804ca10ffdc791fc
cuadchris <cuadradochris@hotmail.com>
2025-01-15 13:30:16 -0500

    Update sum_of_integers.py

--- python/sum_of_integers.py
+++ python/sum_of_integers.py
@@ -8,7 +8,6 @@ string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of th
 
 Note: only positive integers will be tested.
 
-import re
 '''
 
 def sum_of_integers_in_string(s):
