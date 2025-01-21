99b3c315db38f924de5220ca3e5dca1d81408576
cuadchris <cuadradochris@hotmail.com>
2025-01-21 13:03:15 -0500

    Update mario_luigi.py

--- python/mario_luigi.py
+++ python/mario_luigi.py
@@ -14,7 +14,8 @@ Task
 Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from 
 the minimum value up to the maximum value (both included).
 
-Example
+Example:
+
 Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 '''
 
