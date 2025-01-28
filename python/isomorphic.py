c7c3727c1664b507beb95e324bc9e28d26aba767
cuadchris <cuadradochris@hotmail.com>
2025-01-28 10:47:57 -0500

    Update isomorphic.py

--- python/isomorphic.py
+++ python/isomorphic.py
@@ -6,7 +6,6 @@ DESCRIPTION:
 Two strings a and b are called isomorphic if there is a one to one mapping possible for every character 
 of a to every character of b. And all occurrences of every character in a map to same character in b.
 
-Task
 In this kata you will create a function that return True if two given strings are isomorphic to each 
 other, and False otherwise. Remember that order is important.
 
