bbaa23a88b5bb0364c17dee4fceeae7cf14555b3
cuadchris <cuadradochris@hotmail.com>
2025-01-30 10:11:43 -0500

    Update tail_swap.py

--- python/tail_swap.py
+++ python/tail_swap.py
@@ -10,7 +10,7 @@ the colon, are random.
 Your job is to return a list of two strings (in the same order as the original list), 
 but with the characters after each colon swapped.
 
-Examples
+Examples:
 ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
 ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
 '''
