1a3a00e63fe906e04eb4c98b0550ad4c367ec0d5
cuadchris <cuadradochris@hotmail.com>
2025-02-01 11:37:33 -0500

    Update lojban_numbers.py

--- python/lojban_numbers.py
+++ python/lojban_numbers.py
@@ -9,6 +9,7 @@ languages. The numbers from zero to nine are:
 1 pa 4 vo 7 ze
 2 re 5 mu 8 bi 0 no
 3 ci 6 xa 9 so
+
 Larger numbers are created by gluing the digits together. For example, 123 is pareci.
 
 Write a program that reads in a Lojban string (representing a number less than or equal to 1,000,000) and
