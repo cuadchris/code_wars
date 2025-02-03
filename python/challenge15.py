9f59dcf45f817732bc7ffc2a6f4700fb5a031344
cuadchris <cuadradochris@hotmail.com>
2025-02-03 16:12:29 -0500

    Update challenge15.py

--- python/challenge15.py
+++ python/challenge15.py
@@ -12,7 +12,7 @@ to help get some more business.
 The scroller works by replacing the current text string with a similar text string, 
 but with the first letter shifted to the end; this simulates movement.
 
-You're father is far too busy with the business to worry about such details, so, naturally, he's making you come up with the text strings.
+Your father is far too busy with the business to worry about such details, so, naturally, he's making you come up with the text strings.
 
 Create a function named rotate() that accepts a string argument and returns an array of strings 
 with each letter from the input string being rotated to the end.
