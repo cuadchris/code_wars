cdc2804b1ae4bd0b777d7c6ae14116555f171e2c
cuadchris <cuadradochris@hotmail.com>
2025-02-04 12:15:43 -0500

    Update pair_of_gloves.py

--- python/pair_of_gloves.py
+++ python/pair_of_gloves.py
@@ -3,7 +3,6 @@ https://www.codewars.com/kata/58235a167a8cb37e1a0000db/python
 
 DESCRIPTION:
 
-Pair of gloves
 Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine 
 the number of pair of gloves you can constitute from the gloves you have in your drawer.
 
