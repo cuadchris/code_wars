719b642f3e05d7af9a10ef8dd15eb55c0cd0afad
cuadchris <cuadradochris@hotmail.com>
2025-02-14 10:30:08 -0500

    Update sum_of_integers.py

--- python/sum_of_integers.py
+++ python/sum_of_integers.py
@@ -7,7 +7,6 @@ Your task in this kata is to implement a function that calculates the sum of the
 string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
 
 Note: only positive integers will be tested.
-
 '''
 
 def sum_of_integers_in_string(s):
