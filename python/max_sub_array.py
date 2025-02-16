682c0cb7fdb73d2c61ba177d458fb1095e2e5d49
cuadchris <cuadradochris@hotmail.com>
2025-02-16 13:25:48 -0500

    Update max_sub_array.py

--- python/max_sub_array.py
+++ python/max_sub_array.py
@@ -8,6 +8,7 @@ in an array or list of integers:
 
 max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 should be 6: [4, -1, 2, 1]
+
 Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of 
 the whole array. If the list is made up of only negative numbers, return 0 instead.
 
