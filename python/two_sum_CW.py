277821c448b209ef0843a42ca9830ddf6ece6ead
cuadchris <cuadradochris@hotmail.com>
2025-02-18 13:17:42 -0500

    Update two_sum_CW.py

--- python/two_sum_CW.py
+++ python/two_sum_CW.py
@@ -2,6 +2,7 @@
 https://www.codewars.com/kata/52c31f8e6605bcc646000082/python
 
 DESCRIPTION:
+
 Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
 
 For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
