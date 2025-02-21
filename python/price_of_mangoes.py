05487bf4fac20965213748ae1f54fa0a5a756983
cuadchris <cuadradochris@hotmail.com>
2025-02-21 16:06:01 -0500

    Update price_of_mangoes.py

--- python/price_of_mangoes.py
+++ python/price_of_mangoes.py
@@ -6,7 +6,7 @@ DESCRIPTION:
 There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), 
 calculate the total cost of the mangoes.
 
-Examples
+Examples:
 mango(2, 3) ==> 6     2 mangoes for $3 per unit = $6; no mango for free
 mango(3, 3) ==> 6     2 mangoes for $3 per unit = $6; +1 mango for free
 mango(5, 3) ==> 12    4 mangoes for $3 per unit = $12; +1 mango for free
