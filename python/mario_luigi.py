0782302365c17c7bba1caba9663f6e0b2308a664
cuadchris <cuadradochris@hotmail.com>
2025-02-22 14:04:19 -0500

    Update mario_luigi.py

--- python/mario_luigi.py
+++ python/mario_luigi.py
@@ -10,7 +10,6 @@ The pipes connecting your level's stages together need to be fixed before you re
 Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that 
 there is no duplicates.
 
-Task
 Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from 
 the minimum value up to the maximum value (both included).
 
