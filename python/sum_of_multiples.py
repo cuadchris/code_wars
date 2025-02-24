ad7cd31df95affa1dc2f1b16ff17b994d37d8038
cuadchris <cuadradochris@hotmail.com>
2025-02-24 13:02:24 -0500

    Update sum_of_multiples.py

--- python/sum_of_multiples.py
+++ python/sum_of_multiples.py
@@ -7,9 +7,10 @@ Your Job:
 
 Find the sum of all multiples of n below m
 
-Keep in Mind
+Keep in Mind:
 n and m are natural numbers (positive integers)
 m is excluded from the multiples
+
 Examples
 sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
 sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
