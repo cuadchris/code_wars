5760fca1bd53da91809e4ca6fe433a53e1f790e2
cuadchris <cuadradochris@hotmail.com>
2025-02-26 11:22:05 -0500

    Update spinning_rings.py

--- python/spinning_rings.py
+++ python/spinning_rings.py
@@ -16,6 +16,7 @@ e.g. if innerMax is 2 and outerMax is 3 then after
 3 moves: inner = 0, outer = 3
 4 moves: inner = 2, outer = 0
 5 moves: inner = 1, outer = 1
+
 Therefore it takes 5 moves for the two rings to reach the same number
 Therefore spinningRings(2, 3) = 5
 e.g. if innerMax is 3 and outerMax is 2 then after
