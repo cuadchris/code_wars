69f2a9b6429da2449d9f44f6e5c1487b3a8be9aa
cuadchris <cuadradochris@hotmail.com>
2025-03-14 11:26:14 -0400

    Update flick_switch.py

--- python/flick_switch.py
+++ python/flick_switch.py
@@ -4,6 +4,7 @@ https://www.codewars.com/kata/64fbfe2618692c2018ebbddb/python
 DESCRIPTION:
 
 Task:
+
 Create a function that always returns True/true for every item in a given list.
 However, if an element is the word 'flick', switch to always returning the opposite boolean value.
 
