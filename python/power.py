bd23acf5344484fe43814c93fcab0681043300ac
cuadchris <cuadradochris@hotmail.com>
2025-03-15 13:00:57 -0400

    Update power.py

--- python/power.py
+++ python/power.py
@@ -6,7 +6,8 @@ DESCRIPTION:
 The goal is to create a function of two inputs number and power, that "raises" the number up to 
 power (ie multiplies number by itself power times).
 
-Examples
+Examples:
+
 number_to_pwr(3, 2)  # -> 9 ( = 3 * 3 )
 number_to_pwr(2, 3)  # -> 8 ( = 2 * 2 * 2 )
 number_to_pwr(10, 6) # -> 1000000
