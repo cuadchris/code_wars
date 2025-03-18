3c6950a1af9c784effb9e9cd774d8daa0db79280
cuadchris <cuadradochris@hotmail.com>
2025-03-18 15:30:20 -0400

    Update mario_luigi.py

--- python/mario_luigi.py
+++ python/mario_luigi.py
@@ -15,7 +15,8 @@ the minimum value up to the maximum value (both included).
 
 Example:
 
-Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+Input:  1,3,5,6,7,8 
+Output: 1,2,3,4,5,6,7,8
 '''
 
 def pipe_fix(nums):
