bb2fba364ef6332d35d189504a861142f64dca6d
cuadchris <cuadradochris@hotmail.com>
2025-03-19 13:28:26 -0400

    Update sum_of_multiples.py

--- python/sum_of_multiples.py
+++ python/sum_of_multiples.py
@@ -11,7 +11,7 @@ Keep in Mind:
 n and m are natural numbers (positive integers)
 m is excluded from the multiples
 
-Examples
+Examples:
 sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
 sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
 sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
