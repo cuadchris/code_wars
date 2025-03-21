9ea7914d6e51b43e39e55661a232c7ec371b7f6c
cuadchris <cuadradochris@hotmail.com>
2025-03-21 13:12:43 -0400

    Update count_repeats.py

--- python/count_repeats.py
+++ python/count_repeats.py
@@ -9,6 +9,7 @@ with no consecutive repeats.
 Note: This includes any characters
 
 Examples:
+
 'abbbbc'  => 'abc'    #  answer: 3
 'abbcca'  => 'abca'   #  answer: 2
 'ab cca'  => 'ab ca'  #  answer: 1
