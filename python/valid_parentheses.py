d45da7339c1179dce0065f9e506eda15abb9a5ef
cuadchris <cuadradochris@hotmail.com>
2025-03-22 13:27:53 -0400

    Update valid_parentheses.py

--- python/valid_parentheses.py
+++ python/valid_parentheses.py
@@ -6,7 +6,7 @@ DESCRIPTION:
 Write a function that takes a string of parentheses, and determines if the order of the 
 parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
-Examples
+Examples:
 "()"              =>  true
 ")(()))"          =>  false
 "("               =>  false
