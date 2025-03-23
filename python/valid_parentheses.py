ed55e926f0977a02e53f605346a7727ffeaa2ca1
cuadchris <cuadradochris@hotmail.com>
2025-03-23 13:29:28 -0400

    Update valid_parentheses.py

--- python/valid_parentheses.py
+++ python/valid_parentheses.py
@@ -19,7 +19,8 @@ All inputs will be strings, consisting only of characters ( and ).
 Empty strings are considered balanced (and therefore valid), and will be tested.
 For languages with mutable strings, the inputs should not be mutated.
 
-Protip: If you are trying to figure out why a string of parentheses is invalid, 
+Protip:
+If you are trying to figure out why a string of parentheses is invalid, 
 paste the parentheses into the code editor, and let the code highlighting show you!
 '''
 
