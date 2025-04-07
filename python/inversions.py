ff0886dd9ef95f188c8114313a941827b5268b6d
cuadchris <cuadradochris@hotmail.com>
2025-04-07 17:05:13 -0400

    Update inversions.py

--- python/inversions.py
+++ python/inversions.py
@@ -7,7 +7,7 @@ Array inversion indicates how far the array is from being sorted.
 
 Inversions are pairs of elements in array that are out of order.
 
-Examples
+Examples:
 [1, 2, 3, 4]  =>  0 inversions
 [1, 3, 2, 4]  =>  1 inversion: 2 and 3
 [4, 1, 2, 3]  =>  3 inversions: 4 and 1, 4 and 2, 4 and 3
