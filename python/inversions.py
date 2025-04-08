502c4d29e22a8b759905ffa0cf28a3385f8a185a
cuadchris <cuadradochris@hotmail.com>
2025-04-08 15:35:10 -0400

    Update inversions.py

--- python/inversions.py
+++ python/inversions.py
@@ -12,7 +12,7 @@ Examples:
 [1, 3, 2, 4]  =>  1 inversion: 2 and 3
 [4, 1, 2, 3]  =>  3 inversions: 4 and 1, 4 and 2, 4 and 3
 [4, 3, 2, 1]  =>  6 inversions: 4 and 3, 4 and 2, 4 and 1, 3 and 2, 3 and 1, 2 and 1
-Goal
+
 The goal is to come up with a function that can calculate inversions for any arbitrary array
 '''
 
