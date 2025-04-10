9dc56f93e39e08e14b3a494ae345be83e784969c
cuadchris <cuadradochris@hotmail.com>
2025-04-10 16:25:54 -0400

    Update spacify.py

--- python/spacify.py
+++ python/spacify.py
@@ -6,7 +6,8 @@ DESCRIPTION:
 Modify the spacify function so that it returns the given string with spaces inserted between
 each character.
 
-spacify("hello world") # returns "h e l l o   w o r l d"
+spacify("hello world")
+returns "h e l l o   w o r l d"
 '''
 
 def spacify(string):
