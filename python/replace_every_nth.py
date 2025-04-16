9b6494ab46ff14763099e078c8af2a1c3d61a46a
cuadchris <cuadradochris@hotmail.com>
2025-04-16 13:00:56 -0400

    Update replace_every_nth.py

--- python/replace_every_nth.py
+++ python/replace_every_nth.py
@@ -22,9 +22,11 @@ If n is 0 or negative or if it is larger than the count of the oldValue, return 
 original text without a change.
 
 Example:
+
 n:         2
 old_value: 'a'
 new_value: 'o'
+
 "Vader said: No, I am your father!"
    1     2          3        4       -> 2nd and 4th occurence are replaced
 "Vader soid: No, I am your fother!"
