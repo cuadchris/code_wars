aa3b46da412cd4ebd5f74da54c3692407f558447
cuadchris <cuadradochris@hotmail.com>
2025-04-17 07:37:27 -0400

    Update rock_paper_scissors.py

--- python/rock_paper_scissors.py
+++ python/rock_paper_scissors.py
@@ -7,7 +7,7 @@ Rock Paper Scissors
 
 Let's play! You have to return which player won! In case of a draw return Draw!.
 
-Examples(Input1, Input2 --> Output):
+(Input1, Input2 --> Output):
 
 "scissors", "paper" --> "Player 1 won!"
 "scissors", "rock" --> "Player 2 won!"
