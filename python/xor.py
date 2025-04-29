8dda9cb7ac9de401cb0d9500676b63030e71db5c
cuadchris <cuadradochris@hotmail.com>
2025-04-29 15:01:18 -0400

    Update xor.py

--- python/xor.py
+++ python/xor.py
@@ -15,8 +15,6 @@ false xor true == true // exactly one of the two expressions are true
 true xor true == false // Both are true.  "xor" only returns true if EXACTLY one of the two expressions
 evaluate to true.
 
-Task
-
 Since we cannot define keywords in Javascript(well, at least I don't know how to do it), your task is
 to define a function xor(a, b) where a and b are the two
 expressions to be evaluated. Your xor function should have the behaviour described above, returning 
