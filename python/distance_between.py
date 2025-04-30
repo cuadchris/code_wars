2c0aef3527ada836d117e81f6927e603c720edfd
cuadchris <cuadradochris@hotmail.com>
2025-04-30 15:56:24 -0400

    Update distance_between.py

--- python/distance_between.py
+++ python/distance_between.py
@@ -6,7 +6,9 @@ DESCRIPTION:
 The goal of this Kata is to return the greatest distance of index positions between matching 
 number values in an array or 0 if there are no matching values.
 
-Example: In an array with the values [0, 2, 1, 2, 4, 1] the greatest index distance is between 
+Example:
+
+In an array with the values [0, 2, 1, 2, 4, 1] the greatest index distance is between 
 the matching '1' values at index 2 and 5. Executing greatestDistance against this array would return 3.
 
 Here's the previous example in test form:
