141d91836e5fa186ea52844534847ead93e44aa7
cuadchris <cuadradochris@hotmail.com>
2025-05-04 13:14:58 -0400

    Update jenny_youngest_detective.py

--- python/jenny_youngest_detective.py
+++ python/jenny_youngest_detective.py
@@ -15,6 +15,7 @@ Each of the numbers in the array refers to the position of a letter in the strin
 Spaces are not places, you need the actual letters. No spaces.
 The returned word should be all lowercase letters.
 if you can't find one of the letters using the index numbers, return "No mission today". Jenny would be very sad, but that's life... :(
+
 Example: input: [5, 0, 3], "I Love You" output: "ivy" (0 = "i", 3 = "v", 5 = "y")
 '''
 
