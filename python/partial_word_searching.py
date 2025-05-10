f15d299b224e349f51b631491b54d11c8e3620f0
cuadchris <cuadradochris@hotmail.com>
2025-05-10 18:15:37 -0400

    Update partial_word_searching.py

--- python/partial_word_searching.py
+++ python/partial_word_searching.py
@@ -12,6 +12,7 @@ If the string isn't contained in any of the strings in the array, the method ret
 a single string: "Empty" (or Nothing in Haskell, or "None" in Python and C)
 
 Examples:
+
 If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"],
 the method should return ["home", "Mercury"].
 '''
