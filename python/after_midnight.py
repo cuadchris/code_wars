63908ae80c9e6f549af97b3fc3c23e1e1bc1926c
cuadchris <cuadradochris@hotmail.com>
2025-06-03 16:47:45 -0400

    Update after_midnight.py

--- python/after_midnight.py
+++ python/after_midnight.py
@@ -3,7 +3,6 @@ https://www.codewars.com/kata/56fac4cfda8ca6ec0f001746/python
 
 DESCRIPTION:
 
-Instructions
 Write a function that takes a negative or positive integer, which represents the number 
 of minutes before (-) or after (+) Sunday midnight, and returns the current day of the 
 week and the current time in 24hr format ('hh:mm') as a string.
