3e1c805371c2d788008c9649cabd6296598ae7b2
cuadchris <cuadradochris@hotmail.com>
2025-06-06 16:08:41 -0400

    Update growing_plant.py

--- python/growing_plant.py
+++ python/growing_plant.py
@@ -2,7 +2,6 @@
 https://www.codewars.com/kata/58941fec8afa3618c9000184/python
 
 DESCRIPTION:
-
 Each day a plant is growing by upSpeed meters. Each night that plant's height decreases 
 by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. We 
 plant the seed at the beginning of a day. We want to know when the height of the plant 
