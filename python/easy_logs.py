9eb9a8ca7f5a26009999de3aa18eec5232f9ea95
cuadchris <cuadradochris@hotmail.com>
2025-06-11 20:26:18 -0400

    Update easy_logs.py

--- python/easy_logs.py
+++ python/easy_logs.py
@@ -2,7 +2,6 @@
 https://www.codewars.com/kata/5b68c7029756802aa2000176
 
 DESCRIPTION:
-
 Given a logarithm base X and two values A and B, return a sum of logratihms with the base X:
 logxA + logxB.
 
