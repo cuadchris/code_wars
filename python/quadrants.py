b8cd807914e61565f261c44b6fdb8d5ad0e7d6c1
cuadchris <cuadradochris@hotmail.com>
2025-06-14 16:50:07 -0400

    Update quadrants.py

--- python/quadrants.py
+++ python/quadrants.py
@@ -2,7 +2,6 @@
 https://www.codewars.com/kata/643af0fa9fa6c406b47c5399
 
 Description:
-
 Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4
 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
 
